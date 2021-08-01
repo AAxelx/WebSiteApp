3e4db08fdc24b0e6ec3a0f7ad6e6cd88bb7f133d
DESKTOP-JCH77E3\Ananasik <kravchenko.s1111@gmail.com>
2021-08-01 22:18:12 +0300

    Added Abstractions

---
+++ Services/Abstractions/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using WebSiteApp.Models.Requests;
+using WebSiteApp.Models.Responses;
+using System.Threading.Tasks;
+
+namespace WebSiteApp.Services.Abstractions
+{
+    public interface ICartService
+    {
+        Task<AddCartResponse> AddToCart(AddCartRequest addCartRequest);
+        Task<GetCartResponse> GetCart(GetCartRequest getCartRequest);
+        Task<RemoveCartResponse> RemoveFromCart(RemoveCartRequest removeCartRequest);
+        Task<ClearCartResponse> ClearCart(ClearCartRequest clearCartRequest);
+    }
+}
